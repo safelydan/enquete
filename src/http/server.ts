a42582b28856de3eb7f3dd4716d8acbf8849d2b0
daniel araujo <daniellealuau@gmail.com>
2024-02-06 15:19:13 -0300

    primeiros passos

---
+++ src/http/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+
+const port = 3333;
+
+app.get('/hello', (req, res)=>{
+    return 'hello';
+})
+
+app.listen({port}).then(()=>{
+    console.log('http server running');
+})
