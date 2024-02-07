e982a40261419cf769c408db4fc4b0c2f657e400
daniel araujo <daniellealuau@gmail.com>
2024-02-07 15:13:27 -0300

    primeira aula completa

---
+++ polls/readme.md
@@ -0,0 +1,49 @@
+## Configuração do Ambiente de Desenvolvimento
+
+1. **Inicialização do Projeto com TypeScript:**
+   ```bash
+   npx typescript --init
+
+2. **Inicialização do TypeScript:**
+   ```bash
+   npx tsc
+
+3. **Instalação de Dependências do Projeto (Express e Fastify):**
+   ```bash
+    npm install express fastify
+
+4. **Verificar se o Docker está instalado:**
+   ```bash
+    docker --version
+
+5. **Verificar se o Docker Compose está instalado:**
+   ```bash
+    docker-compose --version
+
+6. **Criar e iniciar contêineres definidos no Docker Compose:**
+   ```bash
+    docker-compose up -d
+
+7. **Verificar contêineres em execução:**
+   ```bash
+    docker ps
+
+8. **Inicialização do Prisma:**
+   ```bash
+    npm install @prisma/cli @prisma/client --save-dev
+
+9. **Inicialização do Prisma (Execução de Migrações):**
+   ```bash
+   npx prisma migrate dev
+
+10. **Execuçao do Prisma Studio para ter uma interface:**
+   ```bash
+   npx prisma studio
+
+11. **Usar o HOPPSCOTH:**
+
+12. **Instalar ZOD para validaçao de dados**
+   ```bash
+   npm install zod
+
+
--- polls/src/http/server.ts
+++ polls/src/http/server.ts
@@ -1,17 +1,35 @@
-import fastify from "fastify";
+// importação de módulos
+import fastify from "fastify"; // importa o módulo Fastify para criar o servidor web.
+import { PrismaClient } from "@prisma/client"; // importa o PrismaClient para interação com o banco de dados.
+import { z } from "zod"; // importa a biblioteca Zod para validação de esquema.
 
-const app = fastify();
+const app = fastify(); // cria uma instância do aplicativo Fastify.
+const prisma = new PrismaClient(); // inicializa o Prisma Client para interação com o banco de dados.
+const port = 3333; // porta do servidor.
 
+app.post('/polls', async (req, res) => {
+    // definição do esquema para o corpo da requisição
+    const createPollBody = z.object({
+        title: z.string()
+    });
 
-const port = 3333;
+    // validação do corpo da requisição usando o esquema
+    const { title } = createPollBody.parse(req.body);
 
-app.get('/hello', (req, res)=>{
-    return 'hello';
-})
+    // criação de uma nova enquete no banco de dados usando o Prisma Client
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    });
 
-app.listen({port}).then(()=>{
-    console.log('http server running');
-})
+    // retorna a enquete criada como resposta da requisição com status 201
+    return res.status(201).send({ poll });
+});
 
-// Driver Nativo
-// ORMs
+app.listen({ port }).then(() => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+// comentários adicionais explicando conceitos gerais do código
+// (driver nativo, ORMs, métodos HTTP básicos, etc.)
